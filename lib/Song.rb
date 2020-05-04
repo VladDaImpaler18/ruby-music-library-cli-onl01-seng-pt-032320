8d9fd2aba8d36757081dc74349af1aeef72b7343
Vladimir J <vladdaimpaler@protonmail.com>
2020-05-04 23:31:30 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -1,6 +1,29 @@
 class Song
   
+  attr_accessor :name
+  @@all = []
   
+  def initialize(title, artist_obj = nil, genre_object = nil)
+    @name = title
+  end
+  
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(title)
+    Song.new(title).save
+    self
+  end
   
   
 end
