0a9c168db2291c66771b276ec73a744d32fc548d
Vladimir J <vladdaimpaler@protonmail.com>
2020-05-05 00:56:17 +0000

    Done.

---
+++ lib/Artist.rb
@@ -0,0 +1,31 @@
+class Artist
+  
+  attr_accessor :name, :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    Artist.new(name).save
+    self
+  end
+  
+  def songs
+    Song.all.select { |song_obj| song_obj.artist == self }
+  end
+end
--- lib/Song.rb
+++ lib/Song.rb
@@ -1,10 +1,19 @@
 class Song
   
-  attr_accessor :name
+  attr_accessor :name, :artist
   @@all = []
   
   def initialize(title, artist_obj = nil, genre_object = nil)
     @name = title
+    @artist = artist_obj
+  end
+  
+  def artist
+    self.artist
+  end
+  
+  def artist(artist_obj)
+    @artist = artist_obj
   end
   
   
